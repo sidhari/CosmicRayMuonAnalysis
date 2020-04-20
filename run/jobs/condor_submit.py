8aee0733e4cc4cae1edb38510f24378ebcb3d23f
Siddharth Hariprakash <sidhari@ucla.edu>
2020-04-20 10:16:54 -0700

    Changes to condor submit code

--- run/jobs/condor_submit.py
+++ run/jobs/condor_submit.py
@@ -13,8 +13,8 @@ EXECUTABLE = "corsika77100Linux_QGSII_urqmd" #simulation executable
 OUTPUT_IDENTIFIER = "DAT"
 
 OUTPUT_FOLDER = PROJECT_DIR + "OUTPUTS/" #Outputs go here
-PARTICLE_INFO =  OUTPUT_FOLDER + "PARTICLEINFO/1E6/"
-LONG_INFO = OUTPUT_FOLDER + "LONGINFO/1E6/"
+PARTICLE_INFO =  OUTPUT_FOLDER + "PARTICLEINFO/1E7/"
+LONG_INFO = OUTPUT_FOLDER + "LONGINFO/1E7/"
 SHELL_FOLDER = "condor/" #Condor scripts go here 
 TEMP_RUN_DIR = "condor/run/" #executables go here
 OTHER_DIR = "condor/other/"
@@ -28,44 +28,44 @@ def write_submit_script(script_file, run_folder, input_file_path, output_file):
 
     script_file.write("universe               =vanilla\n")
     script_file.write("executable             =" + (run_folder + RUN_ON_CONDOR_SCRIPT) + "\n")
-    script_file.write("arguments              =" + EXECUTABLE + " " + input_file + " " + output_file + " " + long_output_file + "\n")
+    script_file.write("arguments              =\"" + EXECUTABLE + " " + input_file + " " + output_file + " " + long_output_file + "\"\n")
     script_file.write("output                 =" + (OTHER_DIR + output_file) + ".out\n")
     script_file.write("error                  =" + (OTHER_DIR + output_file) + ".err\n")
     script_file.write("log                    =" + (OTHER_DIR + output_file) + ".log\n")
     script_file.write("should_transfer_files  = YES\n")
-    script_file.write("transfer_input_files   =" + (run_folder + RUN_ON_CONDOR_SCRIPT) + "," + TAR_PATH + "," + input_file_path + "\n")
+    script_file.write("transfer_input_files   =" + (run_folder + RUN_ON_CONDOR_SCRIPT) + "," + TAR_PATH + "\n")
     script_file.write("transfer_output_files  =" + output_file + "," + long_output_file + "\n")
     script_file.write("transfer_output_remaps = \"" + output_file + "=" + PARTICLE_INFO + output_file + ";" + long_output_file + "=" + LONG_INFO + long_output_file + "\"\n")
     script_file.write("whentotransferoutput   =ON_EXIT\n")
     script_file.write("getenv                 =True\n")
+    script_file.write("on_exit_remove         = (ExitCode == 0)\n")
     script_file.write("queue                  1\n")
 
 
 
 #copy executable to the run directory
-#print "Copying executable to the temporary run folder\n"
-#if os.path.isfile(SRC_DIR + EXECUTABLE):
-#    os.system('cp ' + SRC_DIR + EXECUTABLE + ' ' + TEMP_RUN_DIR)
-#else:
-#    print "Could not find executable\n"
-#    exit
+print "\nCopying executable to the temporary run folder\n"
+if os.path.isfile(SRC_DIR + EXECUTABLE):
+    os.system('cp ' + SRC_DIR + EXECUTABLE + ' ' + TEMP_RUN_DIR)
+else:
+    print "Could not find executable\n"
+    exit
 
 #Copy the bash file needed for execution 
-#print "Copying Condor bash file\n"
-#if os.path.isfile(RUN_ON_CONDOR_SCRIPT):
-#    os.system('cp ' + RUN_ON_CONDOR_SCRIPT + ' ' + TEMP_RUN_DIR)
-#else:
-#    print "Could not find the bash script\n"
-#    exit
+print "Copying Condor bash file\n"
+if os.path.isfile(RUN_ON_CONDOR_SCRIPT):
+    os.system('cp ' + RUN_ON_CONDOR_SCRIPT + ' ' + TEMP_RUN_DIR)
+else:
+    print "Could not find the bash script\n"
+    exit
 
-BASE_INPUT_FILE = "../Inputs/1E6/all-inputs_"
+BASE_INPUT_FILE = "../Inputs/1E7/all-inputs_"
 events = 1
 
 for num in range(1,events+1):
 
     INPUT_FILE_PATH = BASE_INPUT_FILE + str(num)
     print "Making Condor script for %s"%INPUT_FILE_PATH
-    print "\n"
 
     OUTPUT_FILE = OUTPUT_IDENTIFIER
 
@@ -84,7 +84,6 @@ for num in range(1,events+1):
     OUTPUT_FILE = OUTPUT_FILE + str(num)
 
     print "Output file name is %s"%OUTPUT_FILE
-    print "\n"
 
     SCRIPT_FILE_NAME = SHELL_FOLDER + OUTPUT_FILE + '.sub'
     SCRIPT_FILE = open(SCRIPT_FILE_NAME,"w")
@@ -92,7 +91,7 @@ for num in range(1,events+1):
     write_submit_script(SCRIPT_FILE, TEMP_RUN_DIR, INPUT_FILE_PATH, OUTPUT_FILE)
     SCRIPT_FILE.close()
 
-    os.system("condor_submit " + SCRIPT_FILE_NAME + " -allow-crlf-script")
+    os.system("condor_submit " + SCRIPT_FILE_NAME)
 
 
 
--- run/jobs/run_on_condor.sh
+++ run/jobs/run_on_condor.sh
@@ -7,24 +7,22 @@ echo "Running on: `uname -a`" #Condor job is running on this node
 echo "System software: `cat /etc/redhat-release`" #Operating System on that node
 echo $CMSVERS
 echo "Copied Files:"
-ls
+
 source /cvmfs/cms.cern.ch/cmsset_default.sh  ## if a tcsh script, use .csh instead of .sh
 export SCRAM_ARCH=slc6_amd64_gcc530
 eval `scramv1 project CMSSW ${CMSVERS}`
 
-cp *.tar.gz $CMSVERS/src
+cp corsika.tar.gz $CMSVERS/src/
 cd $CMSVERS/src/
 eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
-tar xzvf *.tar.gz #untar the package with everything in it
-
-ls -R
+tar xzvf corsika.tar.gz #untar the package with everything in it
 
 cd corsika-77100/run
 
-#executable, inputfile
-./${1}  < Inputs/1E6/${2}
-
 ls -R
+
+#executable, inputfile
+./${1} < Inputs/1E7/${2}
 
 mv ${3} ../../../../ #move out of CMSSW build so it transfers properly
 mv ${4} ../../../../ #move out of CMSSW build so it transfers properly
